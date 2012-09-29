d379a315d17cad58c98515f1c7ed1b9d7138cc8f
Catherine Katherine <catkat@ubuntu.(none)>
2012-09-28 23:57:18 -0400

    HW SEC 1 COM1.2

--- erb_example.rb
+++ erb_example.rb
@@ -1,8 +1,22 @@
 require 'erb'
 
 x = 42
-y = 'cute'
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
-template2 = ERB.new "The animal is: <%= y %>"
-puts template2.result(binding)
+
+y = 'cute'
+template = ERB.new "The animal is: <%= y %>"
+puts template.result(binding)
+
+z = 6
+template = ERB.new "Z^2 = <%= z*z %>"
+puts template.result(binding)
+
+name = 'bruce'
+up_name = "My name is <%=name.upcase %>"
+template = ERB.new(up_name)
+puts template.result(binding)
+
+hash = {Name: "Bruce", Occupation: "Batman", Movie: "TDKR"}
+template = ERB.new"NANANANANANAN <%= hash %>"
+puts template.result(binding)
