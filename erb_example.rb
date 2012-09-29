cae9af5a96f9768b54e1fe4745d1546f9d78c494
Catherine Katherine <catkat@ubuntu.(none)>
2012-09-28 23:39:19 -0400

    HW SEC 1 COM

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,8 @@
 require 'erb'
 
 x = 42
+y = 'cute
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+template2 = ERB.new "The animal is: <%= y %>"
+puts template2.result(binding)
